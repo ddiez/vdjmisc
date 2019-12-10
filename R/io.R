b7c9f47c98113c5ed702450279961adf45dd6542
Diego Diez <diego10ruiz@gmail.com>
2019-12-10 17:20:17 +0900

    Add function read_vdjdb_full().

--- R/io.R
+++ R/io.R
@@ -30,3 +30,36 @@ read_vdj_10x_batch <- function(path = ".", extension = "csv", productive.only = 
   names(vdj) <- f
   bind_rows(vdj, .id = "filename")
 }
+
+
+#' read_vdjdb_full
+#'
+#' @param filename filename.
+#' @param min.score if not NULL, return only entries with this score or greater.
+#' @param paired return only entries with paired (alpha and beta) VJ information.
+#'
+#' @return
+#' @export
+#'
+read_vdjdb_full <- function(filename, min.score = NULL, paired = TRUE) {
+  vdj <- read_tsv(filename)
+
+  vdj <- vdj %>% rename(
+    cdr3.a = cdr3.alpha,
+    v_gene.a = v.alpha,
+    j_gene.a = j.alpha,
+    cdr3.b = cdr3.beta,
+    v_gene.b = v.beta,
+    j_gene.b = j.beta
+  )
+
+  if (!is.null(min.score))
+    vdj <- vdj %>% filter(vdjdb.score >= min.score)
+
+  #vdj <- vdj %>% select("cdr3.a", "v_gene.a", "j_gene.a", "cdr3.b", "v_gene.b", "j_gene.b")
+
+  if (paired)
+    vdj <- vdj %>% tidyr::drop_na("v_gene.a", "j_gene.a", "v_gene.b", "j_gene.b")
+
+  vdj
+}
