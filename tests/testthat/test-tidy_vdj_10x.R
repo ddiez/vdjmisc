a744378dc3e467ba39fe68846dce8b405a9a2894
Diego Diez <diego10ruiz@gmail.com>
2019-12-11 16:43:54 +0900

    Fix unit test for tidy_vdj_10x().

--- tests/testthat/test-tidy_vdj_10x.R
+++ tests/testthat/test-tidy_vdj_10x.R
@@ -11,8 +11,9 @@ test_that("tidy_vdj_10x works", {
 
 path <- file.path(system.file(package = "vdjmisc"), "extdata/10x")
 vdj <- read_vdj_10x_batch(path, extension = "csv.gz")
+vdj <- vdj %>% mutate(barcode = paste(filename, barcode, sep = ":"))
 tidy_vdj <- tidy_vdj_10x(vdj)
 
 test_that("tidy_vdj_10x works on batch", {
-  expect_equal(dim(tidy_vdj), c(75, 37))
+  expect_equal(dim(tidy_vdj), c(82, 37))
 })
