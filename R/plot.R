4754cd097432856c0066c3db11047e890be7e050
Diego Diez <diego10ruiz@gmail.com>
2019-12-11 13:29:29 +0900

    Add split.by argument to plot_vdj_usage().

--- R/plot.R
+++ R/plot.R
@@ -8,6 +8,7 @@
 #'
 #' @param x data.frame
 #' @param cols columns to include.
+#' @param split.by combine and split the vdj plot by this columns.
 #' @param title main title.
 #' @param subtitle subtitlte.
 #' @param size size of text in gene boxes.
@@ -20,7 +21,21 @@
 #'
 #' @export
 #'
-plot_vdj_usage <- function(x, cols = NULL, title = "", subtitle = paste(nrow(x), "sequences"), size = 3, alpha = .4, fill = "violetred", fill.vdj = "limegreen", axis.width = .3, expand.x = 0.1, expand.y = 5) {
+plot_vdj_usage <- function(x, cols = NULL, split.by = NULL, title = "", subtitle = paste(nrow(x), "sequences"), size = 3, alpha = .4, fill = "violetred", fill.vdj = "limegreen", axis.width = .3, expand.x = 0.1, expand.y = 5) {
+
+  if (is.null(split.by)) {
+    plot_vdj_usage_raw(x, cols = cols, title = title, subtitle = subtitle, size = size, alpha = alpha, fill = fill, fill.vdj = fill.vdj, axis.width = axis.width, expand.x = expand.x, expand.y = expand.y)
+  } else {
+    x <- x %>% unite(".group", split.by, sep = "-")
+    x <- split(x, x[[".group"]])
+    pl <- lapply(names(x), function(n) {
+      plot_vdj_usage_raw(x[[n]], cols = cols, title = n, subtitle = subtitle, size = size, alpha = alpha, fill = fill, fill.vdj = fill.vdj, axis.width = axis.width, expand.x = expand.x, expand.y = expand.y)
+    })
+    wrap_plots(pl)
+  }
+}
+
+plot_vdj_usage_raw <- function(x, cols = NULL, title = "", subtitle = paste(nrow(x), "sequences"), size = 3, alpha = .4, fill = "violetred", fill.vdj = "limegreen", axis.width = .3, expand.x = 0.1, expand.y = 5) {
   get_parallel_vdj(x, cols) %>%
     ggplot(
       aes(x = .data$x, split = .data$y, value = .data$n, id = .data$id)) +
