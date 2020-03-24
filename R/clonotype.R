234bf86b5318afb8aae7257ddaa7d385feb3ebc0
Diego Diez <diego10ruiz@gmail.com>
2020-03-24 16:18:32 +0900

    Fix clonotype_size() code and documentation.

--- R/clonotype.R
+++ R/clonotype.R
@@ -1,14 +1,15 @@
 #' clonotype_size
 #'
 #' @param x a tidy VDJ object
+#' @param keep keep this column(s) when reporting clonotype sizes.
 #'
 #' @export
 #'
-clonotype_size <- function(x, by = "barcode") {
+clonotype_size <- function(x, keep = "barcode") {
   x %>%
-    mutate(clonotype = paste(v_gene.a, d_gene.a, j_gene.a, cdr3.a, cdr3.b, v_gene.b, d_gene.b, j_gene.b, sep = "_")) %>%
-    add_count(clonotype) %>%
+    mutate(clonotype = paste(.data$v_gene.a, .data$d_gene.a, .data$j_gene.a, .data$cdr3.a, .data$cdr3.b, .data$v_gene.b, .data$d_gene.b, .data$j_gene.b, sep = "_")) %>%
+    add_count(.data$clonotype) %>%
     arrange(desc(n)) %>%
-    select(by, clonotype, clonotype_size = n)
+    select(keep, .data$clonotype, clonotype_size = .data$n)
 }
 
