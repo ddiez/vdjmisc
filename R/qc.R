f771dd89894e856a149d7b13866f0617474938f9
Diego Diez <diego10ruiz@gmail.com>
2020-03-26 16:21:29 +0900

    Add function add_vdj_qc() that takes a VDJ data.frame and attaches QC info like paired and doublet.

--- R/qc.R
+++ R/qc.R
@@ -26,3 +26,25 @@ compute_vdj_qc <- function(x, barcode.col = "barcode", chain.col = "chain", max.
   doublet %>%
     mutate(doublet = ifelse(.data[["TRA"]] > max.alpha | .data[["TRB"]] > max.beta, TRUE, FALSE))
 }
+
+
+#' add_vdj_qc
+#'
+#' Calculates some qc values for VDJ data including validity (cells must have at least 1 TRA and 1 TRB chain) and
+#' doublet based on the maximum number of TRA and TRB chains (default 1 for both).
+#'
+#' @param x a data.frame with VDJ information.
+#' @param barcode.col which column use as barcode or index.
+#' @param chain.col which column contians the chain information.
+#' @param max.alpha maximum number of alpha chains for singletons.
+#' @param max.beta maximum number of beta chains for singletons.
+#' @param drop_multi drop chain "Multi" from 10x VDJ data.
+#'
+#' @return original data.frame with doublet and paired columns added..
+#' @export
+#'
+add_vdj_qc <- function(x, barcode.col = "barcode", chain.col = "chain", max.alpha = 1, max.beta = 1, drop_multi = TRUE) {
+  qc <- compute_vdj_qc(x, barcode.col = barcode.col, chain.col = chain.col, max.alpha = max.alpha, max.beta = max.beta, drop_multi = drop_multi)
+  qc <- qc %>% select(barcode.col, "paired", "doublet")
+  left_join(x, qc, by = barcode.col)
+}
--- tests/testthat/test-compute_vdj_qc.R
+++ tests/testthat/test-compute_vdj_qc.R
@@ -1,5 +1,5 @@
-d <- data.frame(barcode = c("bc1", "bc1", "bc2", "bc2", "bc2", "bc3", "bc4", "bc5", "bc5"))
-d$chain <- c("TRA", "TRB", "TRA", "TRB", "TRB", "TRA", "TRB", "TRA", "TRA")
+d <- tibble(barcode = c("bc1", "bc1", "bc2", "bc2", "bc2", "bc3", "bc4", "bc5", "bc5"))
+d <- d %>% mutate(chain = c("TRA", "TRB", "TRA", "TRB", "TRB", "TRA", "TRB", "TRA", "TRA"))
 
 x <- compute_vdj_qc(d)
 
@@ -12,4 +12,23 @@ test_that("compute_vdj_qc works", {
   expect_equal(x[["TRB"]], c(1, 2, 0, 1, 0))
   expect_equal(x[["paired"]], c(TRUE, TRUE, FALSE, FALSE, FALSE))
   expect_equal(x[["doublet"]], c(FALSE, TRUE, FALSE, FALSE, TRUE))
+})
+
+
+d <- add_vdj_qc(d)
+
+test_that("add_vdj_qc works", {
+  expect_false(is.null(d))
+  expect_equal(dim(d), c(9, 4))
+  expect_equal(colnames(d), c("barcode", "chain", "paired", "doublet"))
+  expect_equal(d[["paired"]], c(TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE))
+  expect_equal(d[["doublet"]], c(FALSE, FALSE, TRUE, TRUE, TRUE, FALSE, FALSE, TRUE, TRUE))
+})
+
+
+d <- d %>% select(barcode, paired, doublet) %>% distinct() %>% arrange(barcode)
+x <- x %>% select(barcode, paired, doublet) %>% arrange(barcode)
+
+test_that("compute_vdj_qc and add_vdj_qc give identical results", {
+  expect_true(identical(d, x))
 })
